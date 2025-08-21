2537747a9014033e3890b360c8e2e48cd230cad8
qqueiros <quentin.queiros@gmail.com>
2025-08-21 15:50:15 +0200

    New runs

--- code/model/train.py
+++ code/model/train.py
@@ -11,16 +11,16 @@ torch.cuda.empty_cache()
 task = Task.init(project_name="YOLOv11 Training fish-seabird", task_name="YOLOv11 fish-seabird")
 
 # Load a COCO-pretrained YOLO11m model
-model = YOLO("models/auklab_model_nano_fishJuly2025.pt")
+model = YOLO("models/auklab_model_nano_combined_1394_v1.pt")
 
-# Train the model on the dataset for 200 epochs
+# Train the model on the dataset for 30 epochs
 results = model.train(data="dataset/dataset_combined_1394.yaml", batch=32, epochs=200, imgsz=960, device = [0, 1])
 
 # Log the results to ClearML
 task.upload_artifact('training_results_dataset_combined1394', results)
 
 # Save the model
-model.save('models/auklab_model_nano_combined_1394.pt')
+model.save('models/auklab_model_nano_combined_1394_v2.pt')
 
 # Close the ClearML task
 task.close()
